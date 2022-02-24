b13637be461388b779007fe73c8e3c78c22debf9
Tom Booster <tom.booster@gmail.com>
2022-02-23 16:25:19 -0800

    Install xstate and @xstate/vue

---
+++ src/xstate/promise.mjs
@@ -0,0 +1,32 @@
+import { createMachine, interpret } from 'xstate';
+
+const promiseMachine = createMachine({
+  id: 'promise',
+  initial: 'pending',
+  states: {
+    pending: {
+      on: {
+        RESOLVE: { target: 'resolved' },
+        REJECT: { target: 'rejected' }
+      }
+    },
+    resolved: {
+      type: 'final'
+    },
+    rejected: {
+      type: 'final'
+    }
+  }
+})
+
+const promiseService =
+  interpret(promiseMachine)
+    .onTransition((state) => {
+      console.log(
+        'state: ', state.value,
+        ' / event: ', state.event
+      )
+    })
+    .start()  // Start the service
+    
+promiseService.send({ type: 'RESOLVE' });
